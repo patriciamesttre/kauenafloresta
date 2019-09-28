e094b5a7d1f96d80ea1c00ab7dc6e2f9e558efd4
patriciamesttre <patriciamesttre@hotmail.com>
2019-09-27 22:23:52 -0300

    add cena fase1

---
+++ kauenafloresta/Assets/Scripts/Personagem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+
+    public float velocidade = 4;
+    public bool olhandoParaDireita = true;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Mover()
+    {
+        float horizontalForceButton = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalForceButton * velocidade, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        Mover();
+    }
+}
