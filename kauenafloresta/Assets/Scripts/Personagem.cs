b55afddaf5a2d37a51cc517ccdb42d46189ad77a
patriciamesttre <patriciamesttre@hotmail.com>
2019-10-02 22:15:37 -0300

    Criado fase 2 e modificaçoes no script do Personagem

--- kauenafloresta/Assets/Scripts/Personagem.cs
+++ kauenafloresta/Assets/Scripts/Personagem.cs
@@ -8,6 +8,13 @@ public class Personagem : MonoBehaviour
     public float velocidade = 4;
     public bool olhandoParaDireita = true;
 
+    //pulo
+    bool noChao = false;
+    float chaoCheckRaio = 0.2f;
+    public int forcaPulo = 100;
+    public Transform chaoCheck;
+    public LayerMask OQueEChao;
+
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -18,12 +25,35 @@ public class Personagem : MonoBehaviour
 
     void Mover()
     {
+        //Pulo
+        noChao = Physics2D.OverlapCircle(chaoCheck.position, chaoCheckRaio, OQueEChao);
+
         float horizontalForceButton = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalForceButton * velocidade, rb.velocity.y);
+
+        //virar
+        if((horizontalForceButton > 0 && !olhandoParaDireita) || (horizontalForceButton < 0 && olhandoParaDireita)){
+            Virar();
+        }
     }
+
+    void Virar()
+    {
+        olhandoParaDireita = !olhandoParaDireita;
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1;
+        transform.localScale = myScale;
+    }
+        
 
     void Update()
     {
         Mover();
+
+        //pulo
+        if (noChao && Input.GetButton("Jump"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcaPulo));
+        }
     }
 }
