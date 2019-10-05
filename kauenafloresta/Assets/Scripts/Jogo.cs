1d06dbead572f36f4fdca6cb343263aab1639225
patriciamesttre <patriciamesttre@hotmail.com>
2019-10-04 22:15:30 -0300

    Adicionado Pontuaçao e coletar moedas

--- kauenafloresta/Assets/Scripts/Jogo.cs
+++ kauenafloresta/Assets/Scripts/Jogo.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Jogo : MonoBehaviour
 {
+    private int pontos;
+    public Text pontosText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("pontos"))
+        {
+            AtualizarPontos();
+            pontos = PlayerPrefs.GetInt("pontos");
+        }
+    }
+
+    void AtualizarPontos()
+    {
+        pontosText.text = "Pontos: " + PlayerPrefs.GetInt("pontos");
+    }
+
+    public void AdicionarPontos()
+    {
+        pontos++;
+        PlayerPrefs.SetInt("pontos", pontos);
+        AtualizarPontos();
     }
 
     // Update is called once per frame
@@ -16,6 +36,13 @@ public class Jogo : MonoBehaviour
     {
         
     }
+
+    public void CarregaFase1()
+    {
+        PlayerPrefs.SetInt("pontos", 0);
+        SceneManager.LoadScene("Fase1");
+    }
+
 
     public void CarregaIntroducao(){
         SceneManager.LoadScene("Introducao");
---
+++ kauenafloresta/Assets/Scripts/Moeda.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    private Jogo jogo;
+
+
+    private void OnTriggerEnter2D(Collider2D colisor)
+    {
+        if(colisor.gameObject.tag == "Player"){
+            jogo.AdicionarPontos();
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject objetoJogo = GameObject.FindWithTag("JOGO");
+        jogo = objetoJogo.GetComponent<Jogo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
